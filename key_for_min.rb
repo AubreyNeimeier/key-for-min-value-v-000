5ef7c5bfe5e2f5636e1200a64577d27f3543f9af
Aubrey Neimeier <neimeier.1@osu.edu>
2017-12-21 18:56:04 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -37,11 +37,11 @@ end
 def key_for_min(name_hash)
   lowest_value = nil
   lowest_key = nil
-    name_hash.each do |key, value|
-      if lowest_value == nil || value < lowest_value
-        lowest_value = value
-        lowest_key = key
-      end
+  name_hash.each do |key, value|
+    if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      lowest_key = key
     end
+  end
   lowest_key
 end
